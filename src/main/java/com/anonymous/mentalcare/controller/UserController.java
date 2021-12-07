260480d2571f34c2a2ae84ebdcccc01fff453ba3
HundredCleanWater <maxmp9904@gmail.com>
2021-12-07 16:31:08 +0900

    유효성감사

--- src/main/java/com/anonymous/mentalcare/controller/UserController.java
+++ src/main/java/com/anonymous/mentalcare/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     @ApiOperation(value = "기본 회원가입")
     @PostMapping("/api/signup")
