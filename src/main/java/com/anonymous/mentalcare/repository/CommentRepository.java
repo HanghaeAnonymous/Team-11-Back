c69776726a17a2f12763b9322864633b187295fd
HundredCleanWater <90129613+HundredCleanWater@users.noreply.github.com>
2021-12-08 10:59:17 +0900

    Update CommentRepository.java
    댓글 조회

--- src/main/java/com/anonymous/mentalcare/repository/CommentRepository.java
+++ src/main/java/com/anonymous/mentalcare/repository/CommentRepository.java
@@ -9,4 +9,7 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByUser(User user);
+    
+        //생성일자 순 내림차순
+    List<Comment> findAllByCommentIdOrderByCreatedAtDesc(Long commentId);
 }
