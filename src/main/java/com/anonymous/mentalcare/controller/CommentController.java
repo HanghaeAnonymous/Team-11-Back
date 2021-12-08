e592ca8315b0565d6f2c2fbd7bde38ae65ead6dc
Zabee52 <zabeee52@gmail.com>
2021-12-09 00:21:40 +0900

    CommentService Return type ResponseEntity로 변환
    했어요

--- src/main/java/com/anonymous/mentalcare/controller/CommentController.java
+++ src/main/java/com/anonymous/mentalcare/controller/CommentController.java
@@ -1,10 +1,13 @@
 package com.anonymous.mentalcare.controller;
 
 import com.anonymous.mentalcare.dto.CommentRequestDto;
+import com.anonymous.mentalcare.dto.CommentResponseDto;
 import com.anonymous.mentalcare.dto.FeedCommentResponseDto;
 import com.anonymous.mentalcare.security.UserDetailsImpl;
 import com.anonymous.mentalcare.service.CommentService;
+import io.swagger.models.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +21,13 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/api/comments/{postId}")
-    public void comment(@PathVariable Long postId,
-                        @AuthenticationPrincipal UserDetailsImpl userDetails,
-                        @RequestBody CommentRequestDto commentRequestDto){
-        commentService.comment(postId, commentRequestDto, userDetails);
+    public ResponseEntity<CommentResponseDto> comment(@PathVariable Long postId,
+                                                      @AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                      @RequestBody CommentRequestDto commentRequestDto){
+        System.out.println("comment request user : " + userDetails.getUsername());
+        System.out.println("comment : " + commentRequestDto.getComment());
+
+        return commentService.comment(postId, commentRequestDto, userDetails);
     }
         // 특정 댓글 게시글 조회
     @GetMapping("/api/comments/{commentId}")
--- src/main/java/com/anonymous/mentalcare/service/CommentService.java
+++ src/main/java/com/anonymous/mentalcare/service/CommentService.java
@@ -1,6 +1,7 @@
 package com.anonymous.mentalcare.service;
 
 import com.anonymous.mentalcare.dto.CommentRequestDto;
+import com.anonymous.mentalcare.dto.CommentResponseDto;
 import com.anonymous.mentalcare.dto.FeedCommentResponseDto;
 import com.anonymous.mentalcare.models.Comment;
 import com.anonymous.mentalcare.models.Post;
@@ -8,6 +9,8 @@ import com.anonymous.mentalcare.repository.CommentRepository;
 import com.anonymous.mentalcare.repository.PostRepository;
 import com.anonymous.mentalcare.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,14 +23,17 @@ public class CommentService {
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
 
-    public void comment(Long postId, CommentRequestDto commentRequestDto, UserDetailsImpl userDetails) {
+    public ResponseEntity<CommentResponseDto> comment(Long postId, CommentRequestDto commentRequestDto, UserDetailsImpl userDetails) {
         Optional<Post> post = postRepository.findByPostId(postId);
 
         if(!post.isPresent()){
             throw new NullPointerException("유효하지 않거나 이미 삭제된 게시글입니다.");
         }
 
-        commentRepository.save(new Comment(commentRequestDto, userDetails.getUser(), post.get()));
+        Comment comment = commentRepository.save(new Comment(commentRequestDto, userDetails.getUser(), post.get()));
+        CommentResponseDto commentResponseDto = new CommentResponseDto(comment);
+
+        return new ResponseEntity<>(commentResponseDto, HttpStatus.OK);
     }
         //댓글 조회 
     @Transactional
