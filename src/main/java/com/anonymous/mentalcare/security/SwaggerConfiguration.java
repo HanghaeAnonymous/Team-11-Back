48c52685af848a56e56d841e50cb9d53d61ace06
Zabee52 <zabeee52@gmail.com>
2021-12-11 13:02:19 +0900

    Swagger Config import 정리
    했어요

--- src/main/java/com/anonymous/mentalcare/security/SwaggerConfiguration.java
+++ src/main/java/com/anonymous/mentalcare/security/SwaggerConfiguration.java
@@ -3,19 +3,18 @@ package com.anonymous.mentalcare.security;
 import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.*;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
